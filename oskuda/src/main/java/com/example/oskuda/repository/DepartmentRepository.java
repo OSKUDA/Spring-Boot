a67723299f6224f1e374cdd00c570759255f92f1
Oskar Krishna Shrestha <41532513+OSKUDA@users.noreply.github.com>
2023-03-02 09:38:33 +0530

    added: CRUD api

--- oskuda/src/main/java/com/example/oskuda/repository/DepartmentRepository.java
+++ oskuda/src/main/java/com/example/oskuda/repository/DepartmentRepository.java
@@ -2,6 +2,7 @@ package com.example.oskuda.repository;
 
 import com.example.oskuda.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -12,5 +13,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
+    public Department findByDepartmentName(String departmentName);
 
+    public Department findByDepartmentNameIgnoreCase(String departmentName);
 }
