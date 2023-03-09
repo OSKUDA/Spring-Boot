83556a5ed6e0f060a27a1c75e188d528593208c3
oskuda <oscarstha@live.com>
2023-03-09 11:51:10 +0530

    added user entity class

---
+++ security/src/main/java/np/com/oskarshrestha/security/entity/User.java
@@ -0,0 +1,25 @@
+package np.com.oskarshrestha.security.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
