e49c63fbaaf00fb1751f61b9b6e0da2c7981c4d0
oskuda <oscarstha@live.com>
2023-03-09 12:56:34 +0530

    added jwt service to extract username by parsing claims

---
+++ security/src/main/java/np/com/oskarshrestha/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package np.com.oskarshrestha.security.repository;
+
+import np.com.oskarshrestha.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+}
