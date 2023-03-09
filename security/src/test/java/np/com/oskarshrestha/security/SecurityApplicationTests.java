6e16c1458fe4261c17cd475ce1aa536df5542e18
oskuda <oskar4770181@gmail.com>
2023-03-09 11:31:31 +0530

    jwt authentication project

---
+++ security/src/test/java/np/com/oskarshrestha/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.oskarshrestha.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
