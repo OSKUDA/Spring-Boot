70f7204825b666c525102c2ab418f4a9a2bb7bec
oskuda <oscarstha@live.com>
2023-03-29 12:51:01 +0530

    improved: test coverage of auth controller upto 72%

--- login-registration/src/test/java/np/com/oskarshrestha/loginregistration/controller/AuthenticationControllerTest.java
+++ login-registration/src/test/java/np/com/oskarshrestha/loginregistration/controller/AuthenticationControllerTest.java
@@ -2,7 +2,9 @@ package np.com.oskarshrestha.loginregistration.controller;
 
 import np.com.oskarshrestha.loginregistration.model.*;
 import np.com.oskarshrestha.loginregistration.service.UserService;
+import np.com.oskarshrestha.loginregistration.util.ChangeUserPasswordStatus;
 import np.com.oskarshrestha.loginregistration.util.EmailVerificationTokenStatus;
+import np.com.oskarshrestha.loginregistration.util.ForgetPasswordEmailStatus;
 import np.com.oskarshrestha.loginregistration.util.ResendVerifyEmailStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,7 +38,7 @@ class AuthenticationControllerTest {
 
     // tests for AuthenticationController().registerUser
     @Test
-    public void whenNewUserRegisterRequest_returnRegistrationResponseSuccessTrue(){
+    public void whenNewUserRegisterRequest_returnRegistrationResponseSuccessTrue() {
         // arrange
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
@@ -64,14 +66,14 @@ class AuthenticationControllerTest {
     }
 
     @Test
-    public void whenExistingUserRegisterRequest_returnRegistrationResponseSuccessFalse(){
+    public void whenExistingUserRegisterRequest_returnRegistrationResponseSuccessFalse() {
         // arrange
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
         UserRegisterRequest userRegisterRequest = Mockito.mock(UserRegisterRequest.class);
 
         // mock
-        Mockito.when(userService.registerUser(userRegisterRequest,request)).thenReturn(
+        Mockito.when(userService.registerUser(userRegisterRequest, request)).thenReturn(
                 RegistrationResponse.builder()
                         .existingUser(true)
                         .registrationSuccess(false)
@@ -93,7 +95,7 @@ class AuthenticationControllerTest {
 
     // tests for AuthenticationController().verifyEmail
     @Test
-    public void whenStatusIsValid_returnVerifyEmailResponseWithEmailVerificationTokenStatusValid(){
+    public void whenStatusIsValid_returnVerifyEmailResponseWithEmailVerificationTokenStatusValid() {
         // arrange
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
@@ -107,12 +109,12 @@ class AuthenticationControllerTest {
 
         // assertions
         Mockito.verify(userService).verifyEmailToken(token);
-        assertEquals(HttpStatusCode.valueOf(200),response.getStatusCode());
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
         assertEquals(EmailVerificationTokenStatus.VALID, Objects.requireNonNull(response.getBody()).getEmailVerificationTokenStatus());
     }
 
     @Test
-    public void whenStatusIsInvalid_returnVerifyEmailResponseWithEmailVerificationTokenStatusInvalid(){
+    public void whenStatusIsInvalid_returnVerifyEmailResponseWithEmailVerificationTokenStatusInvalid() {
         // arrange
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
@@ -126,12 +128,12 @@ class AuthenticationControllerTest {
 
         // assertions
         Mockito.verify(userService).verifyEmailToken(token);
-        assertEquals(HttpStatusCode.valueOf(200),response.getStatusCode());
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
         assertEquals(EmailVerificationTokenStatus.INVALID, Objects.requireNonNull(response.getBody()).getEmailVerificationTokenStatus());
     }
 
     @Test
-    public void whenStatusIsExpired_returnVerifyEmailResponseWithEmailVerificationTokenStatusExpired(){
+    public void whenStatusIsExpired_returnVerifyEmailResponseWithEmailVerificationTokenStatusExpired() {
         // arrange
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
@@ -145,12 +147,12 @@ class AuthenticationControllerTest {
 
         // assertions
         Mockito.verify(userService).verifyEmailToken(token);
-        assertEquals(HttpStatusCode.valueOf(200),response.getStatusCode());
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
         assertEquals(EmailVerificationTokenStatus.EXPIRED, Objects.requireNonNull(response.getBody()).getEmailVerificationTokenStatus());
     }
 
     @Test
-    public void whenStatusIsDefault_returnVerifyEmailResponseWithEmailVerificationTokenStatusExpired() throws IllegalStateException{
+    public void whenStatusIsDefault_returnVerifyEmailResponseWithEmailVerificationTokenStatusExpired() throws IllegalStateException {
         // arrange
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
@@ -164,13 +166,13 @@ class AuthenticationControllerTest {
 
         // assertions
         Mockito.verify(userService).verifyEmailToken(token);
-        assertEquals(HttpStatusCode.valueOf(400),response.getStatusCode());
+        assertEquals(HttpStatusCode.valueOf(400), response.getStatusCode());
     }
 
     // tests for AuthenticationController().resendVerifyEmail
 
     @Test
-    public void whenNewUser_returnResendVerifyEmailResponseWithResendVerifyEmailStatusSuccess(){
+    public void whenNewUser_returnResendVerifyEmailResponseWithResendVerifyEmailStatusSuccess() {
         // arrange
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
@@ -194,7 +196,7 @@ class AuthenticationControllerTest {
     }
 
     @Test
-    public void whenUserNotFound_returnResendVerifyEmailResponseWithResendVerifyEmailStatusEmailNotRegistered(){
+    public void whenUserNotFound_returnResendVerifyEmailResponseWithResendVerifyEmailStatusEmailNotRegistered() {
         // arrange
         MockHttpServletRequest request = new MockHttpServletRequest();
         RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
@@ -214,6 +216,164 @@ class AuthenticationControllerTest {
         // assertions
         Mockito.verify(userService).resendVerificationEmail(email, request);
         assertEquals(ResendVerifyEmailStatus.EMAIL_NOT_REGISTERED, Objects.requireNonNull(response.getBody()).getResendVerifyEmailStatus());
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+    }
+
+    // tests for AuthenticationController().changePassword
+
+    @Test
+    public void whenInvalidUser_returnChangePasswordResponseWithChangeUserPasswordStatusEmailNotFound() {
+        // arrange
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        String email = "hi@gmail.com";
+        String oldPassword = "123";
+        String newPassword = "456";
+        ChangePasswordRequest changePasswordRequest = ChangePasswordRequest
+                .builder()
+                .email(email)
+                .oldPassword(oldPassword)
+                .newPassword(newPassword)
+                .build();
+
+        // mock
+        Mockito.when(userService.changeUserPassword(
+                        changePasswordRequest.getEmail(),
+                        changePasswordRequest.getOldPassword(),
+                        changePasswordRequest.getNewPassword()
+                ))
+                .thenReturn(ChangeUserPasswordStatus.EMAIL_NOT_FOUND);
+
+        // call method to test
+        ResponseEntity<ChangePasswordResponse> response = authenticationController.changePassword(changePasswordRequest);
+
+        // assertions
+        Mockito.verify(userService).changeUserPassword(
+                changePasswordRequest.getEmail(),
+                changePasswordRequest.getOldPassword(),
+                changePasswordRequest.getNewPassword()
+        );
+        assertEquals(ChangeUserPasswordStatus.EMAIL_NOT_FOUND, Objects.requireNonNull(response.getBody()).getChangeUserPasswordStatus());
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+    }
+
+    @Test
+    public void whenValidUserInvalidOldPassword_returnChangePasswordResponseWithChangeUserPasswordStatusPasswordMismatch() {
+        // arrange
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        String email = "hi@gmail.com";
+        String oldPassword = "123";
+        String newPassword = "456";
+        ChangePasswordRequest changePasswordRequest = ChangePasswordRequest
+                .builder()
+                .email(email)
+                .oldPassword(oldPassword)
+                .newPassword(newPassword)
+                .build();
+
+        // mock
+        Mockito.when(userService.changeUserPassword(
+                        changePasswordRequest.getEmail(),
+                        changePasswordRequest.getOldPassword(),
+                        changePasswordRequest.getNewPassword()
+                ))
+                .thenReturn(ChangeUserPasswordStatus.PASSWORD_MISMATCH);
+
+        // call method to test
+        ResponseEntity<ChangePasswordResponse> response = authenticationController.changePassword(changePasswordRequest);
+
+        // assertions
+        Mockito.verify(userService).changeUserPassword(
+                changePasswordRequest.getEmail(),
+                changePasswordRequest.getOldPassword(),
+                changePasswordRequest.getNewPassword()
+        );
+        assertEquals(ChangeUserPasswordStatus.PASSWORD_MISMATCH, Objects.requireNonNull(response.getBody()).getChangeUserPasswordStatus());
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+    }
+
+    @Test
+    public void whenValidUserValidOldPassword_returnChangePasswordResponseWithChangeUserPasswordStatusSuccess() {
+        // arrange
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        String email = "hi@gmail.com";
+        String oldPassword = "123";
+        String newPassword = "456";
+        ChangePasswordRequest changePasswordRequest = ChangePasswordRequest
+                .builder()
+                .email(email)
+                .oldPassword(oldPassword)
+                .newPassword(newPassword)
+                .build();
+
+        // mock
+        Mockito.when(userService.changeUserPassword(
+                        changePasswordRequest.getEmail(),
+                        changePasswordRequest.getOldPassword(),
+                        changePasswordRequest.getNewPassword()
+                ))
+                .thenReturn(ChangeUserPasswordStatus.SUCCESS);
+
+        // call method to test
+        ResponseEntity<ChangePasswordResponse> response = authenticationController.changePassword(changePasswordRequest);
+
+        // assertions
+        Mockito.verify(userService).changeUserPassword(
+                changePasswordRequest.getEmail(),
+                changePasswordRequest.getOldPassword(),
+                changePasswordRequest.getNewPassword()
+        );
+        assertEquals(ChangeUserPasswordStatus.SUCCESS, Objects.requireNonNull(response.getBody()).getChangeUserPasswordStatus());
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+    }
+
+    // tests for AuthenticationController().forgetPassword
+
+    @Test
+    public void whenUserNotFound_returnForgetPasswordResponseWithForgetPasswordEmailStatusEmailNotFound() {
+        // arrange
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        String email = "hi@gmail.com";
+
+        // mock
+        Mockito.when(userService.forgetPassword(email, request)).thenReturn(ForgetPasswordResponse
+                .builder()
+                .forgetPasswordEmailStatus(ForgetPasswordEmailStatus.EMAIL_NOT_FOUND)
+                .build()
+        );
+
+        // call method to test
+        ResponseEntity<ForgetPasswordResponse> response = authenticationController.forgetPassword(email, request);
+
+        // assertions
+        Mockito.verify(userService).forgetPassword(email, request);
+        assertEquals(ForgetPasswordEmailStatus.EMAIL_NOT_FOUND, Objects.requireNonNull(response.getBody()).getForgetPasswordEmailStatus());
+        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+    }
+
+    @Test
+    public void whenValidUser_returnForgetPasswordResponseWithForgetPasswordEmailStatusSent() {
+        // arrange
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        String email = "hi@gmail.com";
+
+        // mock
+        Mockito.when(userService.forgetPassword(email, request)).thenReturn(ForgetPasswordResponse
+                .builder()
+                .forgetPasswordEmailStatus(ForgetPasswordEmailStatus.SENT)
+                .build()
+        );
+
+        // call method to test
+        ResponseEntity<ForgetPasswordResponse> response = authenticationController.forgetPassword(email, request);
+
+        // assertions
+        Mockito.verify(userService).forgetPassword(email, request);
+        assertEquals(ForgetPasswordEmailStatus.SENT, Objects.requireNonNull(response.getBody()).getForgetPasswordEmailStatus());
         assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
     }
 }
